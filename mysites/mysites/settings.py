e557d026f9079bbe30b7d28f22e446c59ad2ea72
AMartin1987 <alejandramartin@outlook.com>
2023-07-29 22:36:56 +0200

    git commit

--- mysites/mysites/settings.py
+++ mysites/mysites/settings.py
@@ -92,21 +92,18 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         #'URL': os.environ.get('DATABASE_URL'),
         #'NAME': os.environ.get('PGDATABASE'),
-        'NAME': 'myblog',
+        'NAME': 'verceldb',
         #'USER': os.environ.get('PGUSER'),
-        'USER': 'postgres',
+        'USER': 'default',
         #'PASSWORD': os.environ.get('PGPASSWORD'),
-        'PASSWORD': 'Kuraineko',
-        'HOST': 'localhost',
+        'PASSWORD': '4DJbVG5HcMlB',
+        'HOST': 'ep-patient-queen-230316-pooler.eu-central-1.postgres.vercel-storage.com',
         #'HOST': os.environ.get('PGHOST'),
         #'PORT': os.environ.get('PGPORT'),
         'PORT': '5432',
+        
     }
 }
-
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -143,9 +140,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'blog/static/'
-
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'blog/static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'blog/static')
 
 # ckeditor upload path
 
