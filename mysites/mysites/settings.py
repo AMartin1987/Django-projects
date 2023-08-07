0f524c9402a056c4d75796cb1f151b4f252fb702
AMartin1987 <alejandramartin@outlook.com>
2023-08-07 20:19:36 +0200

    git commit

--- mysites/mysites/settings.py
+++ mysites/mysites/settings.py
@@ -91,11 +91,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'URL': os.environ.get('DATABASE_URL'),
-        'NAME': os.environ.get('PGDATABASE'),
-        'USER': os.environ.get('PGUSER'),
-        'PASSWORD': os.environ.get('PGPASSWORD'),
-        'HOST': os.environ.get('PGHOST'),
-        'PORT': os.environ.get('PGPORT'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
         
     }
 }
