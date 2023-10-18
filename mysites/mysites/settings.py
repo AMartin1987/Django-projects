9f0cb1fa3148a04da88dbc5e9b8a97400acaa169
AMartin1987 <alejandramartin@outlook.com>
2023-10-18 13:38:35 +0200

    git commit

--- mysites/mysites/settings.py
+++ mysites/mysites/settings.py
@@ -138,8 +138,8 @@ STATIC_URL = 'blog/static/'
 
 # Vercel
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'blog/static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'blog/staticfiles_build', 'blog/static')
 
 
 # ckeditor upload path
