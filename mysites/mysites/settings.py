52466aee6acb1a57a9db5aa9209b58d447352c0b
AMartin1987 <alejandramartin@outlook.com>
2023-12-21 22:06:34 +0100

    git commit

--- mysites/mysites/settings.py
+++ mysites/mysites/settings.py
@@ -24,7 +24,7 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SITE_ID = 1
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
