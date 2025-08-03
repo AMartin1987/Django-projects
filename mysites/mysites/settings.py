0dcd281a4d04a0edb99d7d5cf9a443be7c08e250
AMartin1987 <alejandramartin@outlook.com>
2025-08-04 01:48:34 +0200

    .

--- mysites/mysites/settings.py
+++ mysites/mysites/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog', 'static')]
 
