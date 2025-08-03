5dc4951a428998406d403b9863c0dcbf4cc1ec7a
AMartin1987 <alejandramartin@outlook.com>
2025-08-03 20:10:57 +0200

    added whitenoise library

--- mysites/mysites/settings.py
+++ mysites/mysites/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,7 +137,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static')]  # Directorio donde tú trabajás
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')  # Donde collectstatic los copiará
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # ckeditor upload path
 
