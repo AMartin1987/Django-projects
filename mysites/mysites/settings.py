ee5547d7c5f6d3d1c0177730aacf385310cff672
AMartin1987 <alejandramartin@outlook.com>
2025-08-03 20:14:11 +0200

    config files

--- mysites/mysites/settings.py
+++ mysites/mysites/settings.py
@@ -22,7 +22,7 @@ env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SITE_ID = 1
 
 # Quick-start development settings - unsuitable for production
@@ -132,6 +132,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')  # carpeta donde se va a guardar collectstatic
 
 STATIC_URL = '/static/'
 
