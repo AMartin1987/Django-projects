faf265d258e4edc3a8c7cc9a20738226ec2f55f4
AMartin1987 <alejandramartin@outlook.com>
2025-08-04 15:40:58 +0200

    removed whitenoise

--- mysites/mysites/wsgi.py
+++ mysites/mysites/wsgi.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysites.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'static'))
+
 app = application  # para Vercel
+
 
 
 
