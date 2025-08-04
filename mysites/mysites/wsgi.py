2fda0b48bc5810185ae29a63899565c3c24f1256
AMartin1987 <alejandramartin@outlook.com>
2025-08-04 20:24:50 +0200

    .

--- mysites/mysites/wsgi.py
+++ mysites/mysites/wsgi.py
@@ -7,17 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import sys
-print("WSGI application starting...", file=sys.stderr)
-
+import os
 from django.core.wsgi import get_wsgi_application
 
-import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysites.settings')
 
 application = get_wsgi_application()
 
-print("WSGI application loaded", file=sys.stderr)
 
 
 
