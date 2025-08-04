7ae0cad9fe6857e05f6ac0ee54cc6f4f7972a59e
AMartin1987 <alejandramartin@outlook.com>
2025-08-04 19:16:03 +0200

    .

--- mysites/mysites/settings.py
+++ mysites/mysites/settings.py
@@ -32,7 +32,7 @@ print(os.path.join(BASE_DIR, 'blog', 'static'))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
--- vercel_wsgi.py
+++ vercel_wsgi.py
@@ -6,3 +6,5 @@ from mysites.wsgi import application
 def handler(environ, start_response):
     return application(environ, start_response)
 
+import sys
+print("WSGI application starting...", file=sys.stderr)
