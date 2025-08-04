097c3833426c96999db47ed6f1991e2b4dc1aed6
AMartin1987 <alejandramartin@outlook.com>
2025-08-04 19:11:31 +0200

    .

--- vercel_wsgi.py
+++ vercel_wsgi.py
@@ -1,4 +1,8 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysites.settings')
+
 from mysites.wsgi import application
 
 def handler(environ, start_response):
     return application(environ, start_response)
+
