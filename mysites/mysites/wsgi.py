abbba12644b4e481bd05aae98543aec9aecc369c
AMartin1987 <alejandramartin@outlook.com>
2025-08-05 00:21:44 +0200

    .

--- mysites/mysites/wsgi.py
+++ mysites/mysites/wsgi.py
@@ -13,10 +13,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysites.settings')
 
 application = get_wsgi_application()
-
-
-
-
-
+app = application
 
 
