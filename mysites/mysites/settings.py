7b6e462c6662769514fc42a30cd847ae01c53276
AMartin1987 <alejandramartin@outlook.com>
2025-08-04 17:35:28 +0200

    .

--- mysites/mysites/settings.py
+++ mysites/mysites/settings.py
@@ -24,6 +24,8 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 SITE_ID = 1
+print("BASE_DIR:", Path(__file__).resolve().parent.parent)
+print("STATIC_ROOT:", os.path.join(Path(__file__).resolve().parent.parent, 'public/static'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
