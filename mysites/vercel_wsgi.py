54d68c87cf07f4ba8c30fbf3175e3c2ff7fce873
AMartin1987 <alejandramartin@outlook.com>
2025-08-04 20:19:29 +0200

    .

--- mysites/vercel_wsgi.py
+++ mysites/vercel_wsgi.py
@@ -1,9 +1,8 @@
 import os
-
 from mysites.wsgi import application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysites.settings')
 
 def handler(environ, start_response):
     return application(environ, start_response)
 
-import sys
-print("WSGI application starting...", file=sys.stderr)
