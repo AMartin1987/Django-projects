8f6741ebbd26e66e555ad52d860ef8fe10690102
AMartin1987 <alejandramartin@outlook.com>
2025-08-04 17:33:00 +0200

    .

--- mysites/mysites/settings.py
+++ mysites/mysites/settings.py
@@ -22,7 +22,7 @@ env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent 
+BASE_DIR = Path(__file__).resolve().parent.parent
 SITE_ID = 1
 
 # Quick-start development settings - unsuitable for production
