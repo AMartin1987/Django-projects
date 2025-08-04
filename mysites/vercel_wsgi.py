20dbfb783ac2a2d81df2518dd14423e98b5baae9
AMartin1987 <alejandramartin@outlook.com>
2025-08-04 23:46:18 +0200

    .

--- mysites/vercel_wsgi.py
+++ mysites/vercel_wsgi.py
@@ -1,8 +1,10 @@
 import os
-from mysites.wsgi import application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysites.settings')
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 
 def handler(environ, start_response):
     return application(environ, start_response)
+
 
