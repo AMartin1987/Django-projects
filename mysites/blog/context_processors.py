7fc4384aa7810a90ba54d50e8a1d5d5998d31eec
AMartin1987 <alejandramartin@outlook.com>
2025-08-05 22:14:46 +0200

    Moved project directory outside of OneDrive sync folder

--- mysites/blog/context_processors.py
+++
@@ -1,9 +0,0 @@
-from .models import Category
-
-
-def add_categories_to_layout(request):
-    category_list = Category.objects.all().order_by('name')[:10].values_list('id', 'name')
-    return {
-        'category_list':category_list,
-    }
-
