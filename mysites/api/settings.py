a7a7d4ff973173c7569764b466c7e12b1cbb9347
AMartin1987 <alejandramartin@outlook.com>
2025-10-10 22:32:24 +0200

    fixed timezone

--- mysites/api/settings.py
+++ mysites/api/settings.py
@@ -23,6 +23,10 @@ load_dotenv()
 env = environ.Env()
 environ.Env.read_env()
 
+#Timezone settings
+TIME_ZONE = 'Europe/Madrid'
+USE_TZ = True
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -33,7 +37,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 
